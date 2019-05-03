4ad0d5fd7226ae2ad057793051296c25b266dd43
DEV3L <jus.beall@gmail.com>
2019-05-03 00:18:47 -0400

    Add test to extract numbers into character array

--- src/ingenious-ocr-machine.test.js
+++ src/ingenious-ocr-machine.test.js
@@ -1,23 +1,3 @@
-/*
-You work for a bank, which has recently purchased an ingenious machine to assist in reading letters and 
-faxes sent in by branch offices. The machine scans the paper documents, and produces a file with a number of 
-entries which each look like this:
-
-```
-    _  _     _  _  _  _  _
-  | _| _||_||_ |_   ||_||_|
-  ||_  _|  | _||_|  ||_| _|
-```
-
-Each entry is 4 lines long, and each line has 27 characters. 
-The first 3 lines of each entry contain an account number written using pipes and underscores, 
-and the fourth line is blank. Each account number should have 9 digits, all of which should be in the range 0-9. 
-A normal file contains around 500 entries.
-
-Your first task is to write a program that can take this file and parse it into actual account numbers.
-
-*/
-
 const assert = require('assert');
 
 const IngeniousOCRMachine = require('./ingenious-ocr-machine');
@@ -26,13 +6,13 @@ describe('Ingenious OCR Machine', function() {
   it('OCR all zeros', function() {
     const expectedAccount = '000000000';
     // prettier-ignore
-    const zeros =
+    const accountCharacters =
       " _  _  _  _  _  _  _  _  _ \n" +
       "| || || || || || || || || |\n" +
       "|_||_||_||_||_||_||_||_||_|\n"
 
     const ocrMachine = new IngeniousOCRMachine();
-    const account = ocrMachine.read(zeros);
+    const account = ocrMachine.read(accountCharacters);
 
     assert.equal(expectedAccount, account);
   });
@@ -40,13 +20,27 @@ describe('Ingenious OCR Machine', function() {
   it('OCR all ones', function() {
     const expectedAccount = '111111111';
     // prettier-ignore
-    const ones = 
+    const accountCharacters = 
     "                           \n" +
     "|  |  |  |  |  |  |  |  |  \n" +
     "|  |  |  |  |  |  |  |  |  \n"
 
     const ocrMachine = new IngeniousOCRMachine();
-    const account = ocrMachine.read(ones);
+    const account = ocrMachine.read(accountCharacters);
+
+    assert.equal(expectedAccount, account);
+  });
+
+  it('OCR one and rest zeros', function() {
+    const expectedAccount = '100000000';
+    // prettier-ignore
+    const accountCharacters =
+      "    _  _  _  _  _  _  _  _ \n" +
+      "|  | || || || || || || || |\n" +
+      "|  |_||_||_||_||_||_||_||_|\n"
+
+    const ocrMachine = new IngeniousOCRMachine();
+    const account = ocrMachine.read(accountCharacters);
 
     assert.equal(expectedAccount, account);
   });
