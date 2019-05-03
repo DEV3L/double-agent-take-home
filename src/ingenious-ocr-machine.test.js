69cd25770d2d03c0a1f8eb5aa13de2b4e109a055
DEV3L <jus.beall@gmail.com>
2019-05-03 01:44:27 -0400

    Remove unnecessary unit tests

--- src/ingenious-ocr-machine.test.js
+++ src/ingenious-ocr-machine.test.js
@@ -3,62 +3,6 @@ const assert = require('assert');
 const IngeniousOCRMachine = require('./ingenious-ocr-machine');
 
 describe('Ingenious OCR Machine', function() {
-  it('OCR all zeros', function() {
-    const expectedAccount = '000000000';
-    // prettier-ignore
-    const accountCharacters =
-      " _  _  _  _  _  _  _  _  _ \n" +
-      "| || || || || || || || || |\n" +
-      "|_||_||_||_||_||_||_||_||_|\n"
-
-    const ocrMachine = new IngeniousOCRMachine();
-    const account = ocrMachine.read(accountCharacters);
-
-    assert.equal(expectedAccount, account);
-  });
-
-  it('OCR all ones', function() {
-    const expectedAccount = '111111111';
-    // prettier-ignore
-    const accountCharacters =
-    "                           \n" +
-    "  |  |  |  |  |  |  |  |  |\n" +
-    "  |  |  |  |  |  |  |  |  |\n"
-
-    const ocrMachine = new IngeniousOCRMachine();
-    const account = ocrMachine.read(accountCharacters);
-
-    assert.equal(expectedAccount, account);
-  });
-
-  it('OCR one and rest zeros', function() {
-    const expectedAccount = '100000000';
-    // prettier-ignore
-    const accountCharacters =
-      "    _  _  _  _  _  _  _  _ \n" +
-      "  || || || || || || || || |\n" +
-      "  ||_||_||_||_||_||_||_||_|\n"
-
-    const ocrMachine = new IngeniousOCRMachine();
-    const account = ocrMachine.read(accountCharacters);
-
-    assert.equal(expectedAccount, account);
-  });
-
-  it('OCR two and rest zeros', function() {
-    const expectedAccount = '200000000';
-    // prettier-ignore
-    const accountCharacters =
-      " _  _  _  _  _  _  _  _  _ \n" +
-      " _|| || || || || || || || |\n" +
-      "|_ |_||_||_||_||_||_||_||_|\n"
-
-    const ocrMachine = new IngeniousOCRMachine();
-    const account = ocrMachine.read(accountCharacters);
-
-    assert.equal(expectedAccount, account);
-  });
-
   it('OCR numbers', function() {
     const expectedAccount = '123456789';
     // prettier-ignore
