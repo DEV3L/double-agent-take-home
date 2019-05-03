39bcfaad68dcf449a11e6f6764c05dd0f2ceb8bb
DEV3L <jus.beall@gmail.com>
2019-05-03 01:34:20 -0400

    Complete requirements for story one

--- src/ingenious-ocr-machine.test.js
+++ src/ingenious-ocr-machine.test.js
@@ -59,17 +59,17 @@ describe('Ingenious OCR Machine', function() {
     assert.equal(expectedAccount, account);
   });
 
-  // it('OCR numbers', function() {
-  //   const expectedAccount = '123456789';
-  //   // prettier-ignore
-  //   const accountCharacters =
-  //     "    _  _     _  _  _  _  _ \n" +
-  //     "  | _| _||_||_ |_   ||_||_|\n" +
-  //     "  ||_  _  || _||_  |||_| _|\n"
+  it('OCR numbers', function() {
+    const expectedAccount = '123456789';
+    // prettier-ignore
+    const accountCharacters =
+      "    _  _     _  _  _  _  _ \n" +
+      "  | _| _||_||_ |_   ||_||_|\n" +
+      "  ||_  _|  | _||_|  ||_| _|\n"
 
-  //   const ocrMachine = new IngeniousOCRMachine();
-  //   const account = ocrMachine.read(accountCharacters);
+    const ocrMachine = new IngeniousOCRMachine();
+    const account = ocrMachine.read(accountCharacters);
 
-  //   assert.equal(expectedAccount, account);
-  // });
+    assert.equal(expectedAccount, account);
+  });
 });
--- src/ocr-characters.js
+++ src/ocr-characters.js
@@ -33,12 +33,52 @@ const four = [
   "  |"
  ]
 
+// prettier-ignore
+const five = [
+  " _ ",
+  "|_ ",
+  " _|"
+ ]
+
+// prettier-ignore
+const six = [
+  " _ ",
+  "|_ ",
+  "|_|"
+ ]
+
+// prettier-ignore
+const seven = [
+  " _ ",
+  "  |",
+  "  |"
+ ]
+
+// prettier-ignore
+const eight = [
+  " _ ",
+  "|_|",
+  "|_|"
+ ]
+
+// prettier-ignore
+const nine = [
+  " _ ",
+  "|_|",
+  " _|"
+ ]
+
 const orgCharacters = {
   0: zero,
   1: one,
   2: two,
   3: three,
   4: four,
+  5: five,
+  6: six,
+  7: seven,
+  8: eight,
+  9: nine,
 };
 
 module.exports = orgCharacters;
